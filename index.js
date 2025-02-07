baad9e2c82e9ddd59198ef887dcb156527664f4e
KaaD <jemirouseche@hotmail.com>
2025-02-06 22:07:53 -0500

    🚀Fixing Deployment

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ import cors from 'cors';
 
 const app = express();
 
-const server = http.createServer(app);
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://front-app-disp.vercel.app/'
+];
+
+app.use(cors({ origin: allowedOrigins, methods: ['GET', 'POST'] }));
+
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: allowedOrigins,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
   },
 });
-
-app.use(cors({ origin: 'http://localhost:5173', methods: ['GET', 'POST'] }));
 
 io.on('connection', (socket) => {
   console.log('Cliente conectado');
