8863a309dc4febb53b9b21e97faabed3da940768
KaaD <jemirouseche@hotmail.com>
2025-02-06 22:13:09 -0500

    🐛Bug fixes

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://front-app-disp.vercel.app/'
+  'https://front-app-disp.vercel.app/' 
 ];
 
 app.use(cors({ origin: allowedOrigins, methods: ['GET', 'POST'] }));
 
+
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
@@ -21,20 +24,20 @@ const io = new Server(server, {
   },
 });
 
+
 io.on('connection', (socket) => {
-  console.log('Cliente conectado');
+  console.log(' Cliente conectado');
 
   socket.on('sendLocation', (locationData) => {
-    console.log('Datos recibidos del cliente:', locationData);
+    console.log(' Datos recibidos del cliente:', locationData);
     socket.emit('locationUpdate', locationData);
   });
 
   socket.on('disconnect', () => {
-    console.log('Cliente desconectado');
+    console.log(' Cliente desconectado');
   });
 });
 
-const port = 3000;
 server.listen(port, () => {
-  console.log(`Servidor WebSockets corriendo en http://localhost:${port}`);
+  console.log(` Servidor WebSockets corriendo en el puerto ${port}`);
 });
